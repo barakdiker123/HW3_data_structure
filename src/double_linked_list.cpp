d27fc2aba18162bff7252583616bf4075713c845
barak diker <barakdiker@gmail.com>
2022-11-11 19:53:06 +0200

    another HW

---
+++ src/double_linked_list.cpp
@@ -0,0 +1,106 @@
+
+
+#include <catch2/catch_test_macros.hpp>
+#include <cstddef>
+#include <optional>
+#include <string>
+struct double_linked_list {
+  int id;
+  std::string name;
+  double_linked_list *prev;
+  double_linked_list *next;
+  double_linked_list(int id, std::string name, double_linked_list *prev,
+                     double_linked_list *next)
+      : id(id), name(name), prev(prev), next(next) {}
+};
+
+struct HeadDLL {
+  double_linked_list *head;
+  double_linked_list *tail;
+  HeadDLL(double_linked_list *head) : head(head), tail(head) {}
+  void insert_front(double_linked_list *data) {
+    if (head == nullptr) {
+      head = data;
+      return;
+    }
+    data->next = head;
+    head->prev = data;
+    head = data;
+  }
+  void insert_back(double_linked_list *data) {
+    if (head == nullptr) {
+      head = data;
+      return;
+    }
+    double_linked_list *ptr = head;
+    while (ptr->next != nullptr) {
+      ptr = ptr->next;
+    }
+    data->prev = ptr->next;
+    ptr->next = data;
+  }
+  void insert_back(int id, std::string name) {
+    double_linked_list *element =
+        new double_linked_list(id, name, nullptr, nullptr);
+    if (head == nullptr) {
+      head = element;
+    }
+    insert_back(element);
+  }
+  double_linked_list *search(int id) {
+    if (head == nullptr)
+      return nullptr; // The element has not been found
+    if (head->id == id)
+      return head;
+    double_linked_list *ptr = head;
+    while (ptr->next != nullptr) {
+      if (ptr->id == id) {
+        return ptr;
+      }
+      ptr = ptr->next;
+    }
+    return nullptr; // The element has not been found
+  }
+};
+
+TEST_CASE("Testing double linked list", "[doube_link_list]") {
+  double_linked_list l1 = {1, "barak", nullptr, nullptr};
+  double_linked_list l2 = {2, "ariel", nullptr, nullptr};
+  double_linked_list l3 = {3, "moshea", nullptr, nullptr};
+
+  HeadDLL head(&l1);
+
+  head.insert_back(&l2);
+  head.insert_back(&l3);
+
+  REQUIRE(head.head->id == 1);
+  REQUIRE(head.head->next->id == 2);
+  REQUIRE(head.head->next->next->id == 3);
+}
+
+TEST_CASE("search DDL", "[double_link_list]") {
+  double_linked_list l1 = {1, "barak", nullptr, nullptr};
+  double_linked_list l2 = {2, "ariel", nullptr, nullptr};
+  double_linked_list l3 = {3, "moshea", nullptr, nullptr};
+  HeadDLL head(&l1);
+  head.insert_back(&l2);
+  head.insert_back(&l3);
+  REQUIRE(head.search(1)->name == "barak");
+  REQUIRE(head.search(2)->name == "ariel");
+  REQUIRE(head.search(4) == nullptr);
+}
+TEST_CASE("Search single element", "[ddl]") {
+  double_linked_list l1 = {1, "barak", nullptr, nullptr};
+  HeadDLL head(&l1);
+  REQUIRE(head.search(1)->name == "barak");
+  REQUIRE(head.search(2) == nullptr);
+  REQUIRE(head.search(4) == nullptr);
+}
+TEST_CASE("Search single stufff asdf asf a", "[ddl]") {
+  HeadDLL head(nullptr);
+  double_linked_list l1 = {1, "barak", nullptr, nullptr};
+  head.insert_front(&l1);
+}
+
+//(set 'barak '(1 2 "barak"))
+//(message "Hello %s %s" "World" (nth 2 barak))
--- src/queue.cpp
+++ src/queue.cpp
@@ -1,37 +1,70 @@
 //#include "queue.h"
-
 #include <array>
 #include <catch2/catch_test_macros.hpp>
+#include <optional>
 
 template <typename T, int N> class Queue {
 private:
   std::array<T, N> arr;
-  int last = 0;
   int first = 0;
+  int last = 0;
 
 public:
-  explicit Queue() {
-    first = 0;
-    last = 0;
-  }
-  std::array<T, N> &get_arr() { return arr; }
-  bool is_empty() { return 0 == last ? true : false; }
+  explicit Queue() {}
+  std::array<T, N> &get_arr() { return arr; } // for testing !
+  int get_first() { return first; }           // for testing !!
+  int get_last() { return last; }             // for testing !!
+  bool is_empty() { return 0 == last - first ? true : false; }
   /**
    * @brief insert element into the queue
    * @param T var = the var want to insert
    * @returns true = if empty , false = if not empty
-   *
-   *
    * */
   bool enqueue(T var) {
-    if (last > N)
+    if (last >= N)
       return false;
-    arr[++last] = var;
+    arr[last++] = var;
     return true;
+  }
+  std::optional<T> dequeue() {
+    if ((first >= N) || (first >= last)) {
+      return std::nullopt; /// FAILED
+    }
+    return arr[first++];
   }
 };
 
 TEST_CASE("Queue Check", "[queue]") {
-  REQUIRE(2 == 2);
-  REQUIRE(1 == 1);
+  Queue<int, 10> queue;
+  REQUIRE(queue.is_empty() == true);
+  for (int i = 0; i < 10; i++) {
+    queue.enqueue(i);
+  }
+  REQUIRE(queue.get_arr()[0] == 0);
+  REQUIRE(queue.enqueue(10) == false);
+  REQUIRE(queue.enqueue(10) == false);
+
+  REQUIRE(queue.dequeue() == 0);
+  REQUIRE(queue.dequeue() == 1);
+}
+TEST_CASE("Queue pop", "[queue]") {
+  Queue<int, 10> queue;
+  for (int i = 0; i < 5; i++) {
+    queue.enqueue(i);
+  }
+  REQUIRE(queue.dequeue() == 0);
+  REQUIRE(queue.dequeue() == 1);
+  queue.enqueue(100);
+  REQUIRE(queue.dequeue() == 2);
+  REQUIRE(queue.dequeue() == 3);
+  REQUIRE(queue.dequeue() == 4);
+  REQUIRE(queue.dequeue() == 100);
+  INFO("First is " << queue.get_first());
+  INFO("Last is " << queue.get_last());
+  queue.dequeue();
+  INFO("First is " << queue.get_first());
+  INFO("Last is " << queue.get_last());
+  queue.enqueue(1000);
+  REQUIRE(queue.dequeue() == 1000);
+  REQUIRE(queue.dequeue() == std::nullopt);
 }
